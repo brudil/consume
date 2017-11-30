72c59fa1283f0af564e3820daba1cd54eb71ce83
James Canning <james@brudil.com>
2017-11-30 22:52:31 +0000

    init

---
+++ src/ApplicationRoot.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Application extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>consume</h1>
+        <h1>social drinking games</h1>
+      </div>
+    );
+  }
+}
